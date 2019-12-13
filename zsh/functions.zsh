65ce7424342036621777d6a80d5d1e68a0c5d6e1
kumaran-14 <kumarangowrisankar@yahoo.in>
2019-12-13 14:31:25 +0530

    Add zsh functions

---
+++ zsh/functions.zsh
@@ -0,0 +1,13 @@
+################### ZSH FUNCTIONS ###################
+
+# for "cd ..../dir"
+rationalise-dot() {
+  if [[ $LBUFFER = *.. ]]; then
+    LBUFFER+=/..
+  else
+    LBUFFER+=.
+  fi
+}
+
+zle -N rationalise-dot
+bindkey . rationalise-dot
