e511377412cd55f66edcebfdf611da04811b2b79
kumaran-14 <kumarangowrisankar@yahoo.in>
2019-12-13 13:57:41 +0530

    Move forgit plugin to git

---
+++ git/aliases.zsh
@@ -0,0 +1,6 @@
+################## GIT ALIASES #######################
+
+# forgit aliases defined in $DOTDIR/git/forgit.zsh
+#glo - interactive git log with fzf preview and bat
+#ga - interactive git add
+#gd - interactive git diff
