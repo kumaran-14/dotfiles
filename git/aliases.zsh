b8a409969b7dafd6d7e9eabf335bcf3c8864684a
kumaran-14 <kumarangowrisankar@yahoo.in>
2020-01-04 19:49:58 +0530

    Add tmux module, and add git-completions

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -4,3 +4,11 @@
 #glo - interactive git log with fzf preview and bat
 #ga - interactive git add
 #gd - interactive git diff
+
+
+#from git-extra-commands
+alias gitroot='cd $(git rev-parse --show-toplevel) && echo "$_"'
+alias git-cdroot='cd $(git rev-parse --show-toplevel) && echo "$_"'
+
+#More aliases
+alias gs='git status'
--- git/git-extra-commands.plugin.zsh
+++ git/git-extra-commands.plugin.zsh
@@ -16,41 +16,33 @@
 #PLUGIN_D=$(dirname "$0")
 #export PATH="${PATH}:${PLUGIN_D}/bin"
 
-alias git-grab='git-incoming-commits'
-alias gitroot='cd $(git rev-parse --show-toplevel) && echo "$_"'
-alias git-cdroot='cd $(git rev-parse --show-toplevel) && echo "$_"'
 
 # Skipped:
 #   incoming-commits  (appears to be a dupe of grab)
 #   mark-all-resolved (git: 'conflicts' is not a git command.)
 zstyle ':completion:*:*:git:*' user-commands \
+  churn:'List files in ref with change/commit count' \
+  divergence:'List local/remote (incoming/outgoing) changes for current branch' \
+  incoming:'Fetch remote tracking branch, and list incoming commits' \
+  outgoing:'Fetch remote tracking branch, and list outgoing commits' \
+  undo-push:'Undo your last push to branch ($1) of origin' \
+  where:'Shows where a particular commit falls between releases' \
+  winner:'Determines "winner" by commit count, and number of lines' \
+  wtf:'Displays the state of your repository in a readable, easy-to-scan format' \
+  #prune-branches:'Simple script that cleans up unnecessary branches' \
+  #upstream-sync:'Sync to upstream/yourforkname and rebase into your local fork, then push'
   #age:'A git-blame viewer, written using PyGTK written by Kristoffer Gronlund ' \
   #big-file:'List disk size of files in ref' \
   #change-author:'Rewrite commits, updating author/email' \
   #changes:'List authors/emails with commit count' \
-  churn:'List files in ref with change/commit count' \
-  #copy-branch-name:'Copy the current branch name to the clipboard (pbcopy)' \
-  #credit:'A very slightly quicker way to credit an author on the latest commit' \
-  #current-branch:'Print the name of the current branch, helpful for automation' \
-  #cut-branch:'Create a new named branch pointed at HEAD and reset the current branch to the head of its tracking branch' \
-  #delete-local-merged:'Delete all local branches that have been merged into HEAD' \
-  divergence:'List local/remote (incoming/outgoing) changes for current branch' \
-  #find-dirty:'Recurse current directory, listing "dirty" git clones' \
-  #flush:'Recompactify your repo to be as small as possible' \
-  #grab:'Add github remote, by username and repo' \
-  #ignored:'List files currently being ignored by .gitignore' \
-  #improved-merge:'Sophisticated git merge with integrated CI check and automatic cleanup upon completion' \
-  incoming:'Fetch remote tracking branch, and list incoming commits' \
-  #ls-object-refs:'Find references to <object> SHA1 in refs, commits, and trees. All of them' \
-  #maildiff: 'Simple git command to email diff in color to reviewer/co-worker & optionally attach patch file.' \
-  #maxpack:'Repack with maximum compression' \
-  #nuke:'Nukes a branch locally and on the origin remote' \
-  #object-deflate:'Deflate an loose object file and write to standard output' \
-  outgoing:'Fetch remote tracking branch, and list outgoing commits' \
+  #unpushed:"Show the diff of everything you haven't pushed yet" \
+  #unpushed-stat:"Show the diffstat of everything you haven't pushed yet" \
+  #unreleased:'Shows git commits since the last tagged version' \
+  #up-old:'Like git-pull but show a short and sexy log of changes immediately after merging (git-up) or rebasing (git-reup)' \
+  #when-merged:'Find when a commit was merged into one or more branches' \
   #pie-ify:'Apply perl pie, on the fly' \
   #promote:'Promotes a local topic branch to a remote tracking branch of the same name, by pushing and then setting up the git config' \
   #pruneall:'Prune branches from specified remotes, or all remotes when <remote> not specified' \
-  #prune-branches:'Simple script that cleans up unnecessary branches' \
   #publish:'Pushes/publishes current branch to specified remote' \
   #purge-from-history:'Script to permanently delete files/folders from your git repository' \
   #rank-contributors:"A simple script to trace through the logs and rank contributors by the total size of the diffs they're responsible for" \
@@ -65,13 +57,18 @@ zstyle ':completion:*:*:git:*' user-commands \
   #thanks:'List authors with commit count' \
   #track:'Sets up your branch to track a remote branch' \
   #trail:'Show all branching points in Git history' \
-  undo-push:'Undo your last push to branch ($1) of origin' \
-  #unpushed:"Show the diff of everything you haven't pushed yet" \
-  #unpushed-stat:"Show the diffstat of everything you haven't pushed yet" \
-  #unreleased:'Shows git commits since the last tagged version' \
-  #up-old:'Like git-pull but show a short and sexy log of changes immediately after merging (git-up) or rebasing (git-reup)' \
-  #upstream-sync:'Sync to upstream/yourforkname and rebase into your local fork, then push' \
-  #when-merged:'Find when a commit was merged into one or more branches' \
-  where:'Shows where a particular commit falls between releases' \
-  winner:'Determines "winner" by commit count, and number of lines' \
-  wtf:'Displays the state of your repository in a readable, easy-to-scan format'
+  #ls-object-refs:'Find references to <object> SHA1 in refs, commits, and trees. All of them' \
+  #maildiff: 'Simple git command to email diff in color to reviewer/co-worker & optionally attach patch file.' \
+  #maxpack:'Repack with maximum compression' \
+  #nuke:'Nukes a branch locally and on the origin remote' \
+  #object-deflate:'Deflate an loose object file and write to standard output' \
+  #find-dirty:'Recurse current directory, listing "dirty" git clones' \
+  #flush:'Recompactify your repo to be as small as possible' \
+  #grab:'Add github remote, by username and repo' \
+  #ignored:'List files currently being ignored by .gitignore' \
+  #improved-merge:'Sophisticated git merge with integrated CI check and automatic cleanup upon completion' \
+  #find-dirty:'Recurse current directory, listing "dirty" git clones' \
+  #flush:'Recompactify your repo to be as small as possible' \
+  #grab:'Add github remote, by username and repo' \
+  #ignored:'List files currently being ignored by .gitignore' \
+  #improved-merge:'Sophisticated git merge with integrated CI check and automatic cleanup upon completion' \
