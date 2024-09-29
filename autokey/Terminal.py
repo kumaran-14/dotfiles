66f26489ca658519449d1d7d132ba885e1d1dc51
kumaran-14 <kumarangowrisankar@yahoo.in>
2024-09-28 19:28:14 -0500

    add autokey and todo: ssh key name update

---
+++ autokey/Terminal.py
@@ -0,0 +1,22 @@
+import subprocess
+
+# Execute the command using subprocess
+command = 'wmctrl -lx'
+output = subprocess.check_output(command, shell=True, text=True)
+
+# Check if Firefox is running
+if 'Konsole' in output:
+    # Get the active window class
+    active_win_class_command = "xdotool getactivewindow getwindowclassname"
+    winClass = subprocess.check_output(active_win_class_command, shell=True, text=True).strip()
+
+    # If the active window is Firefox, minimize it
+    if winClass == 'Konsole':
+        subprocess.run("xdotool windowminimize $(xdotool getactivewindow)", shell=True)
+    else:
+        # Otherwise, activate Firefox
+        subprocess.run("wmctrl -xa Konsole", shell=True)
+else:
+    # If Firefox is not running, start it
+    #subprocess.Popen(["/usr/bin/firefox"])
+    pass
